40b4acf27f4e10d28159697bc115703c59198e2d
ragnar <ragnarhardarson90@gmail.com>
2025-04-20 08:53:46 +0200

    Render a super simple 3d shape

--- DogPoopTracker/ContentView.swift
+++ DogPoopTracker/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import RealityKit
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Poop Shape Selector")
+                .font(.title)
+                .padding()
+            ConeView()
+                .frame(height: 300)
+                .background(.orange)
+
+            Spacer()
         }
-        .padding()
+    }
+}
+
+struct ConeView: View {
+    var body: some View {
+        RealityView { content in
+            let mesh = MeshResource.generateCone(height: 1, radius: 0.1)
+            let material = SimpleMaterial(color: .brown, roughness: 0.7, isMetallic: false)
+            let poopEntity = ModelEntity(mesh: mesh, materials: [material])
+
+            let anchorEntity = AnchorEntity()
+            anchorEntity.position = SIMD3<Float>(0, 0, -0.5)
+            anchorEntity.addChild(poopEntity)
+
+            poopEntity.move(to: Transform(), relativeTo: anchorEntity)
+
+            content.add(anchorEntity)
+        }
     }
 }
 
