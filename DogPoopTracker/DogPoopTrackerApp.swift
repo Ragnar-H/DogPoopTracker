2306c9dd631adc8f61d680bd37e613eb584e1a8a
ragnar <ragnarhardarson90@gmail.com>
2025-04-20 07:48:53 +0200

    Initial Commit

---
+++ DogPoopTracker/DogPoopTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPoopTrackerApp.swift
+//  DogPoopTracker
+//
+//  Created by Ragnar on 20/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct DogPoopTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DogPoopTrackerTests/DogPoopTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DogPoopTrackerTests.swift
+//  DogPoopTrackerTests
+//
+//  Created by Ragnar on 20/04/2025.
+//
+
+import Testing
+@testable import DogPoopTracker
+
+struct DogPoopTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
