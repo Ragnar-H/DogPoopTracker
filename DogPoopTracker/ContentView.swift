1b0cc087e8fa9161d0ab6e4413436e4b6c361e83
ragnar <ragnarhardarson90@gmail.com>
2025-05-03 15:59:37 +0200

    Render 5 boxes into view

--- DogPoopTracker/ContentView.swift
+++ DogPoopTracker/ContentView.swift
@@ -6,16 +6,23 @@
 //
 
 import SwiftUI
+import RealityKit
 
 struct ContentView: View {
+    let itemSpacing:Float = 0.6
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        RealityView { content in
+            for i in 0..<5 {
+                let entity = ModelEntity(mesh: .generateBox(size: [0.4, 0.4, 0.1]), materials: [SimpleMaterial(color: .red, isMetallic: false)])
+
+                let targetX = Float(i) * itemSpacing
+                entity.position = [targetX, 0, 0]
+                entity.name = "box-\(i)"
+
+                content.add(entity)
+            }
         }
-        .padding()
     }
 }
 
