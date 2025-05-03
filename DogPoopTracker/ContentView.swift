8aa964fd4493ad8f3efc7cb3e74e1d1a4e6aee99
ragnar <ragnarhardarson90@gmail.com>
2025-05-03 16:00:07 +0200

    Make boxes draggable

--- DogPoopTracker/ContentView.swift
+++ DogPoopTracker/ContentView.swift
@@ -10,6 +10,7 @@ import RealityKit
 
 struct ContentView: View {
     let itemSpacing:Float = 0.6
+    @State private var dragOffset:CGFloat = 0
 
     var body: some View {
         RealityView { content in
@@ -22,7 +23,29 @@ struct ContentView: View {
 
                 content.add(entity)
             }
+        } update: { content in
+            let normalizedDragOffset = Float(dragOffset) * 0.003
+            for i in 0..<5 {
+                if let itemEntity = content.entities.first(where: { $0.name == "box-\(i)"}) as? ModelEntity {
+                    let indexOffset = Float(i - 3) + normalizedDragOffset
+                    let targetX = indexOffset * itemSpacing
+                    // Feels kind of fiddly to calculate position in the render(update) call
+                    // would it be better to calculate and update positions in
+                    // ObservableObject state vars. Then use the render (update)
+                    // call to have smooth animations
+                    itemEntity.position = [targetX, 0, 0]
+                }
+
+            }
+
+
         }
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    dragOffset = value.translation.width
+                }
+        )
     }
 }
 
